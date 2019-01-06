b0efc86d7158b6b7f75184d0a177e2ff78aa440b
Bruno Campos <brunosopmac@gmail.com>
2019-01-06 15:25:07 -0200

    comentando o código para explicar o objetivo das functions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+// importando a função anônima e atribuindo a uma const
 const funcao = require('./callback');
 
-// função síncrona
+// função recebendo os dois parâmetros enviados pela função callback: string e numero
 funcao(function(string, numero) {
     console.log(`2. Resposta: ${string}`);
     
--- callback.js
+++ callback.js
@@ -1,7 +1,16 @@
 module.exports = function(callback) {
+    /**
+     * Função anônima passando uma outra função como parâmetro (callback);
+     * @param {function} - callback
+     */
     console.log('1. Executando function sincrona');
 
     setTimeout(() => {
+        /**
+         * Função callback retornando dois parâmetros:
+         * @param {string} - 'callback executado'
+         * @param {number} - 10
+         */
         callback('Callback executado', 10);
     }, 5000);
 }
