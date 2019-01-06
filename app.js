2f49a665c569c07e862436fe0c1b9ce2b31548b2
Bruno Campos <brunosopmac@gmail.com>
2019-01-06 16:46:29 -0200

    relembrando o básico de REST

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const app = require('express')();
+
+app.listen('5000', (req, res) => {
+    console.log('Servidor Iniciado');
+});
+
+app.get('/', (req, res) => {
+    res.send(req.headers);
+});
+
+
 // importando a função anônima e atribuindo a uma const
 const funcao = require('./callback');
 
